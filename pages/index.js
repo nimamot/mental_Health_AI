026b5bde92cca52e26d9156b4e91d4d1b433283f
nimamot <64922998+nimamot@users.noreply.github.com>
2023-02-21 23:12:17 -0800

    done with output text area

--- pages/index.js
+++ pages/index.js
@@ -50,13 +50,25 @@ const Home = () => {
             value={userInput}
             onChange={onUserChangedText} />
           <div className="prompt-buttons">
-            <a className="generate-button" onClick={callGenerateEndpoint}>
+            <a className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}>
               <div className="generate">
-                <p>Generate</p>
+                {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
               </div>
             </a>
           </div>
-
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="badge-container grow">
